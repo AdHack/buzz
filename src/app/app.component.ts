6b4ceb156f0a02e140b9203213c7693d1489e3e8
Amy 杰 <amy@adhackers.club>
2017-06-03 14:32:51 -0700

    Removed fluff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,34 +4,20 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { UserAuth } from '../providers/user-auth';
-
-import { RegisterPage } from '../pages/register/register';
+import { HomePage } from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   
-  rootPage:any = RegisterPage;
+  rootPage: any = HomePage;
 
-  constructor(	private userAuth: UserAuth,	
-  				private platform: Platform, 
+  constructor(	private platform: Platform, 
   				private statusBar: StatusBar, 
   				private splashScreen: SplashScreen) {
     platform.ready().then(() => {
-    	/* Set root page dependent on user's login status */
-    	if(this.userAuth.isLoggedIn()) {
-    		console.log("User has existing login");
-    		this.rootPage = RegisterPage;
-    	} else if(userAuth.hasRegistered()) {
-    		console.log("User is registered but not logged in");
-    		this.rootPage = RegisterPage;
-    	} else {
-    		console.log("User has not registered");
-    		this.rootPage = RegisterPage;
-    	}
-
+    	
     	/* And away we go! */
     	statusBar.styleDefault();
     	splashScreen.hide();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,9 @@ import { NativePageTransitions } from '@ionic-native/native-page-transitions';
 
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-import { RegisterPage } from '../pages/register/register';
-import { LoadingPage } from '../pages/loading/loading';
 
 import { Data } from '../providers/data';
-import { UserAuth } from '../providers/user-auth';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -32,12 +26,7 @@ const cloud_settings: CloudSettings = {
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    RegisterPage,
-    LoadingPage,
-    TabsPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -48,18 +37,12 @@ const cloud_settings: CloudSettings = {
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    RegisterPage,
-    LoadingPage,
-    TabsPage
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     Data,
-    UserAuth,
     SQLite,
     NativePageTransitions,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
